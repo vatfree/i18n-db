4144a37bf6a08bbbc7e4c2f474f5216afddc48a7
soskarsson <siggi@vatfree.com>
2020-12-01 16:39:38 +0100

    Forked and changed dependency to vatfree package

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  name: 'tap:i18n-db',
+  name: 'vatfree:i18n-db',
   summary: 'Internationalization for Meteor Collections',
-  version: '0.4.0',
-  git: 'https://github.com/TAPevents/tap-i18n-db'
+  version: '1.0.0',
+  git: 'https://github.com/vatfree/tap-i18n-db'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1');
+  api.versionsFrom('1.6');
 
   api.use(["coffeescript", "underscore", "meteor", "jquery", "reactive-dict"], ['server', 'client']);
 
   api.use("autopublish", ['server', 'client'], {weak: true})
 
-  api.use('tap:i18n@1.0.3', ['client', 'server']);
-  api.imply('tap:i18n', ['client', 'server']);
+  api.use('vatfree:i18n@1.31.0', ['client', 'server']);
+  api.imply('vatfree:i18n', ['client', 'server']);
 
   api.use('yogiben:admin@1.1.0', {weak: true});
 
